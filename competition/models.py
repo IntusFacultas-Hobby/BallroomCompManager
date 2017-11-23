36f4c4f79e0822e289022c930fe73831698b5552
Pedro Del Moral Lopez <pdelmora@purdue.edu>
2017-11-22 19:02:27 -0500

    creating schema

--- competition/models.py
+++ competition/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from competitor.models import Dancer
 # Create your models here.
 
 class Competition(models.Model):
@@ -17,3 +17,16 @@ class Event(models.Model):
     max_per_heat = models.IntegerField("Max Couples per Heat", default=0)
     def __str__(self):
         return self.name
+
+class Couple(models.Model):
+    lead = models.ForeignKey(Dancer, on_delete=models.CASCADE, related_name="is_lead")
+    follow = models.ForeignKey(Dancer, on_delete=models.CASCADE,related_name="is_follow")
+    event = models.ManyToManyField(Event, related_name="couples")
+
+
+class FinalistPlace(models.Model):
+    '''
+    FinalistPlace is my solution to potential multiple couples being placed at the same position. An event can have many FinalistPlaces, and the place of each FinalistPlace will be automatically calculated based on scores.
+    '''
+    place = models.IntegerField("Place")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
